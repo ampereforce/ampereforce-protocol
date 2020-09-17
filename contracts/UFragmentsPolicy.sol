059ff2771903cea0601265abed7a9b8b30018661
AmpereForce <ampereforce@hotmailpro.org>
2020-09-17 16:21:34 +0800

    Add the crypto index change ratio.

--- contracts/UFragmentsPolicy.sol
+++ contracts/UFragmentsPolicy.sol
@@ -31,6 +31,7 @@ contract UFragmentsPolicy is Ownable {
         uint256 indexed epoch,
         uint256 exchangeRate,
         uint256 cpi,
+        uint256 cryptoRate,
         int256 requestedSupplyAdjustment,
         uint256 timestampSec
     );
@@ -44,6 +45,17 @@ contract UFragmentsPolicy is Ownable {
     // (eg) An oracle value of 1.5e18 it would mean 1 Ample is trading for $1.50.
     IOracle public marketOracle;
 
+    // Crypto oracle provides the main cryptocurrency market value daily change as an 18 decimal fixed point number.
+    // (eg) An oracle value of 1.5e18 it would mean the main cryptocurrency market value has increased 50% for the last day.
+    // (eg) An oracle value of 0.8e18 it would mean down 20%.
+    IOracle public cryptoOracle;
+
+    // the main cryptocurrency market value daily changement influence percentage weight
+    // as an 18 decimal fixed point number.
+    // (eg) An cryptoWeight value of 0.5e18 means 50%.
+    // (eg) An cryptoWeight value of 0.2e18 means 20%.
+    uint256 public cryptoWeight;
+
     // CPI value at the time of launch, as an 18 decimal fixed point number.
     uint256 private baseCpi;
 
@@ -56,9 +68,11 @@ contract UFragmentsPolicy is Ownable {
     // The rebase lag parameter, used to dampen the applied supply adjustment by 1 / rebaseLag
     // Check setRebaseLag comments for more details.
     // Natural number, no decimal places.
+    // rebaseLag= 10
     uint256 public rebaseLag;
 
     // More than this much time must pass between rebase operations.
+    // 24hours
     uint256 public minRebaseTimeIntervalSec;
 
     // Block timestamp of last rebase operation
@@ -66,9 +80,12 @@ contract UFragmentsPolicy is Ownable {
 
     // The rebase window begins this many seconds into the minRebaseTimeInterval period.
     // For example if minRebaseTimeInterval is 24hrs, it represents the time of day in seconds.
+    // 7200sec = 2hours 2:00 am
+    // 2:00 AM (2:00) International
     uint256 public rebaseWindowOffsetSec;
 
     // The length of the time window where a rebase operation is allowed to execute, in seconds.
+    // 20min
     uint256 public rebaseWindowLengthSec;
 
     // The number of rebase cycles since inception
@@ -81,6 +98,8 @@ contract UFragmentsPolicy is Ownable {
     uint256 private constant MAX_RATE = 10**6 * 10**DECIMALS;
     // MAX_SUPPLY = MAX_INT256 / MAX_RATE
     uint256 private constant MAX_SUPPLY = ~(uint256(1) << 255) / MAX_RATE;
+    // crypto rate limit to (0-100%)
+    uint256 private constant MAX_CRYPTO_RATE_ORIGINAL = 2 * 10**DECIMALS;
 
     // This module orchestrates the rebase execution and downstream notification.
     address public orchestrator;
@@ -111,13 +130,16 @@ contract UFragmentsPolicy is Ownable {
 
         uint256 cpi;
         bool cpiValid;
+        // cpi=110.21e18
         (cpi, cpiValid) = cpiOracle.getData();
         require(cpiValid);
 
+        // targetRate=1.003239393939394e+18
         uint256 targetRate = cpi.mul(10 ** DECIMALS).div(baseCpi);
 
         uint256 exchangeRate;
         bool rateValid;
+        // exchangeRage=0.7694186438892614e+18
         (exchangeRate, rateValid) = marketOracle.getData();
         require(rateValid);
 
@@ -125,7 +147,17 @@ contract UFragmentsPolicy is Ownable {
             exchangeRate = MAX_RATE;
         }
 
-        int256 supplyDelta = computeSupplyDelta(exchangeRate, targetRate);
+        uint256 cryptoRate = 1 * 10 ** DECIMALS;
+        bool cryptoRateValid;
+        (cryptoRate, cryptoRateValid) = cryptoOracle.getData();
+        require(cryptoRateValid);
+
+        // cryptoRate-1 limit to (-1, 1), means (-100%, 100%)
+        if (cryptoRate > MAX_CRYPTO_RATE_ORIGINAL) {
+            cryptoRate = MAX_CRYPTO_RATE_ORIGINAL;
+        }
+
+        int256 supplyDelta = computeSupplyDelta(exchangeRate, targetRate, cryptoRate);
 
         // Apply the Dampening factor.
         supplyDelta = supplyDelta.div(rebaseLag.toInt256Safe());
@@ -136,7 +168,7 @@ contract UFragmentsPolicy is Ownable {
 
         uint256 supplyAfterRebase = uFrags.rebase(epoch, supplyDelta);
         assert(supplyAfterRebase <= MAX_SUPPLY);
-        emit LogRebase(epoch, exchangeRate, cpi, supplyDelta, now);
+        emit LogRebase(epoch, exchangeRate, cpi, cryptoRate, supplyDelta, now);
     }
 
     /**
@@ -159,6 +191,21 @@ contract UFragmentsPolicy is Ownable {
         onlyOwner
     {
         marketOracle = marketOracle_;
+    }
+
+    function setCryptoOracle(IOracle cryptoOracle_)
+        external
+        onlyOwner
+    {
+        cryptoOracle = cryptoOracle_;
+    }
+
+    function setCryptoWeight(uint256 cryptoWeight_)
+        external
+        onlyOwner
+    {
+        require(cryptoWeight_ <= 1 * 10 ** DECIMALS);
+        cryptoWeight = cryptoWeight_;
     }
 
     /**
@@ -250,7 +297,10 @@ contract UFragmentsPolicy is Ownable {
         epoch = 0;
 
         uFrags = uFrags_;
+        // baseCpi_ = 100e18
         baseCpi = baseCpi_;
+        // cryptoWeight = 50% = 0.5e18 = 50e16
+        cryptoWeight = 50 * 10 ** (DECIMALS-2);
     }
 
     /**
@@ -264,41 +314,32 @@ contract UFragmentsPolicy is Ownable {
         );
     }
 
-    /**
-     * @return Computes the total supply adjustment in response to the exchange rate
-     *         and the targetRate.
-     */
-    function computeSupplyDelta(uint256 rate, uint256 targetRate)
+    function computeSupplyDelta(uint256 rate, uint256 targetRate, uint256 cryptoRate)
         private
         view
         returns (int256)
     {
-        if (withinDeviationThreshold(rate, targetRate)) {
+        int256 oneUnit = (10 ** DECIMALS).toInt256Safe();
+        int256 cryptoRateSigned = cryptoRate.toInt256Safe().sub(oneUnit);
+        int256 rateSigned = rate.toInt256Safe();
+        int256 targetRateSigned = targetRate.toInt256Safe();
+        int256 totalSupply = uFrags.totalSupply().toInt256Safe();
+
+        // supplyDeltaRate1 = (10**18 - cryptoWeight) * (rate - targetRate) / targetRate
+        int256 supplyDeltaRate1 = oneUnit.sub(cryptoWeight.toInt256Safe())
+            .mul(rateSigned.sub(targetRateSigned)).div(targetRateSigned);
+
+        // supplyDeltaRate2 = cryptoWeight * cryptoRate / 10**18
+        int256 supplyDeltaRate2 = cryptoWeight.toInt256Safe().mul(cryptoRateSigned).div(oneUnit);
+
+        int256 supplyDeltaRateResult = supplyDeltaRate1.add(supplyDeltaRate2);
+
+        if (supplyDeltaRateResult >= 0 && supplyDeltaRateResult < deviationThreshold.toInt256Safe()) {
             return 0;
         }
-
-        // supplyDelta = totalSupply * (rate - targetRate) / targetRate
-        int256 targetRateSigned = targetRate.toInt256Safe();
-        return uFrags.totalSupply().toInt256Safe()
-            .mul(rate.toInt256Safe().sub(targetRateSigned))
-            .div(targetRateSigned);
-    }
-
-    /**
-     * @param rate The current exchange rate, an 18 decimal fixed point number.
-     * @param targetRate The target exchange rate, an 18 decimal fixed point number.
-     * @return If the rate is within the deviation threshold from the target rate, returns true.
-     *         Otherwise, returns false.
-     */
-    function withinDeviationThreshold(uint256 rate, uint256 targetRate)
-        private
-        view
-        returns (bool)
-    {
-        uint256 absoluteDeviationThreshold = targetRate.mul(deviationThreshold)
-            .div(10 ** DECIMALS);
-
-        return (rate >= targetRate && rate.sub(targetRate) < absoluteDeviationThreshold)
-            || (rate < targetRate && targetRate.sub(rate) < absoluteDeviationThreshold);
+        if (supplyDeltaRateResult < 0 && supplyDeltaRateResult.abs() < deviationThreshold.toInt256Safe()) {
+            return 0;
+        }
+        return totalSupply.mul(supplyDeltaRateResult).div(oneUnit);
     }
 }
